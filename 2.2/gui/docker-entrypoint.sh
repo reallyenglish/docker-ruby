0afa47eec9ddc338306fda12f6e27b9d43ba15c1
ochko <ochkol@reallyenglish.com>
2017-03-06 15:26:43 +0900

    fix entrypoint script for escaped commands
    to support commands containing double quotes like `bash -c "cd subdir &&
    bundle exec rspec spec"`

--- 2.2/gui/docker-entrypoint.sh
+++ 2.2/gui/docker-entrypoint.sh
@@ -4,7 +4,7 @@ export DISPLAY=:99
 PIDFILE=/tmp/custom_xvfb_99.pid
 
 /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-exec $*
+exec "${@}"
 EXITCODE=$?
 /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
 exit $EXITCODE
